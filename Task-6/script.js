ff3d631b576683cd8179d07bb5c041727af89f8c
renu-aayush-maddi <2200032294@kluniversity.in>
2026-03-30 21:44:04 +0530

    task-6 updated(added colour effects)

--- Task-6/script.js
+++ Task-6/script.js
@@ -5,16 +5,31 @@ items.forEach(item => {
 
 item.addEventListener("dragstart",function() {
     dragged = item 
+    item.classList.add("dragging")
 })
 
 item.addEventListener("dragover",function(e) {
-        e.preventDefault()
+    e.preventDefault()
 })
+
+item.addEventListener("dragend",function() {
+    item.classList.remove("dragging")
+})
+
 
 item.addEventListener("drop",function() {
     let temp = dragged.textContent
     dragged.textContent = item.textContent
     item.textContent = temp
+
+    item.classList.add("dropped")
+    dragged.classList.add("dropped")
+
+    setTimeout(() =>{
+    item.classList.remove("dropped")
+    dragged.classList.remove("dropped")
+    },500)
+    
 })
 
 })
